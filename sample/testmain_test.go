07c51145ce1a33009056e2347866501b8bbaa9c9
champon1020 <nagatelu1020@gmail.com>
2023-05-22 18:51:42 +0900

    add ci

--- sample/testmain_test.go
+++ sample/testmain_test.go
@@ -1,7 +1,6 @@
 package sample_test
 
 import (
-	"log"
 	"testing"
 
 	"github.com/champon1020/go-spanner-emulator-sample/testutil"
@@ -13,9 +12,4 @@ func TestMain(m *testing.M) {
 	}
 
 	m.Run()
-
-	log.Println("DONE")
-	// if err := testutil.DropDatabases(); err != nil {
-	// 	panic(err)
-	// }
 }
